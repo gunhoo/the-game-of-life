b1fad6f361248af2e66b0a3e3f0e7b06258dc107
ZHU GUANGYU <zgy@ZHUdeMacBook-Pro.local>
2017-11-23 18:52:31 +0900

    update algorithm which uses list

--- GameOfLife/GameOfLife/Save.cpp
+++ GameOfLife/GameOfLife/Save.cpp
@@ -2,30 +2,30 @@
 
 using namespace std;
 
-int Save::saveFile(string fileName, vector <vector< bool >> grid, int row, int col) {
+int Save::saveFile(string fileName, vector <vector< bool > > grid, int row, int col) {
 	int x, y;
-
-	//GUI ±¸Çö 
-
+	
+	//GUI ï¿½ï¿½ï¿½ï¿½ 
+	
 	//if save button clicked Gui will ask for filename and it will be send to here
-
+	
 
 	//if textfile is already exists than show error message or ask overwrite? return 0;
 
 	//open file
-
+	
 	fstream file;
 	file.open(fileName, ios_base::out | ios_base::in);  // will not create file
 	if (file.is_open())
 	{
-
+		
 		//Gui
 		cout << "Warning, file already exists";
-
+		
 		file.close();
 		return 0;
 	}
-
+	
 	ofstream myfile;
 	myfile.open(fileName);
 
@@ -42,7 +42,7 @@ int Save::saveFile(string fileName, vector <vector< bool >> grid, int row, int c
 			else {
 				myfile << "-";
 			}
-
+			
 		}
 		myfile << "\n";
 	}

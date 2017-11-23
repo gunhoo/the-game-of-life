e5902cbe8e25736902f38e13124dcee004d32c86
gunhoo <gunhoo2016@naver.com>
2017-11-24 00:34:36 +0900

    some GUIs are fixed from display bracn to display (#4) (Resolved)
    * modify size of buttons
    
    * set Initial View
    
    * change type of InitialView to QDialog and set modal of MainView
    
    * add file dialog with Initial View
    
    * success to load file with filename from initial view
    
    * add handler to menu buttons then implement quit / load
    
    * some buttons are added at mainView
    
    some buttons are added at mainView and modify size of the main view
    
    * change resizeButton & UI in the mainView
    
    change resizeButton & UI in the mainView
    add gameLayout

--- GameOfLife/GameOfLife/GeneratedFiles/ui_gameoflifeview.h
+++ GameOfLife/GameOfLife/GeneratedFiles/ui_gameoflifeview.h
@@ -13,11 +13,14 @@
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpinBox>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QToolBar>
 #include <QtWidgets/QWidget>
@@ -33,8 +36,19 @@ public:
     QAction *actionSave_As;
     QAction *actionQuit;
     QWidget *centralWidget;
+    QWidget *horizontalLayoutWidget;
+    QHBoxLayout *horizontalLayout;
+    QLabel *label;
+    QSpinBox *universeSizeX;
+    QLabel *label_2;
+    QSpinBox *universeSizeY;
+    QPushButton *resizeButton;
+    QWidget *horizontalLayoutWidget_2;
+    QHBoxLayout *horizontalLayout_2;
     QPushButton *runButton;
     QPushButton *nextButton;
+    QPushButton *stopButton;
+    QPushButton *clearButton;
     QMenuBar *menuBar;
     QMenu *menuFile;
     QToolBar *mainToolBar;
@@ -44,9 +58,10 @@ public:
     {
         if (GameOfLifeViewClass->objectName().isEmpty())
             GameOfLifeViewClass->setObjectName(QStringLiteral("GameOfLifeViewClass"));
-        GameOfLifeViewClass->resize(800, 450);
-        GameOfLifeViewClass->setMinimumSize(QSize(800, 450));
+        GameOfLifeViewClass->resize(450, 450);
+        GameOfLifeViewClass->setMinimumSize(QSize(450, 450));
         GameOfLifeViewClass->setMaximumSize(QSize(2000, 1500));
+        GameOfLifeViewClass->setAutoFillBackground(false);
         actionNew = new QAction(GameOfLifeViewClass);
         actionNew->setObjectName(QStringLiteral("actionNew"));
         actionOpen = new QAction(GameOfLifeViewClass);
@@ -59,20 +74,96 @@ public:
         actionQuit->setObjectName(QStringLiteral("actionQuit"));
         centralWidget = new QWidget(GameOfLifeViewClass);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
-        runButton = new QPushButton(centralWidget);
+        horizontalLayoutWidget = new QWidget(centralWidget);
+        horizontalLayoutWidget->setObjectName(QStringLiteral("horizontalLayoutWidget"));
+        horizontalLayoutWidget->setGeometry(QRect(10, 0, 431, 41));
+        horizontalLayout = new QHBoxLayout(horizontalLayoutWidget);
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        horizontalLayout->setContentsMargins(0, 0, 0, 0);
+        label = new QLabel(horizontalLayoutWidget);
+        label->setObjectName(QStringLiteral("label"));
+        label->setMinimumSize(QSize(25, 30));
+        QFont font;
+        font.setPointSize(12);
+        font.setBold(false);
+        font.setWeight(50);
+        label->setFont(font);
+
+        horizontalLayout->addWidget(label);
+
+        universeSizeX = new QSpinBox(horizontalLayoutWidget);
+        universeSizeX->setObjectName(QStringLiteral("universeSizeX"));
+        universeSizeX->setMinimumSize(QSize(100, 30));
+        universeSizeX->setValue(40);
+
+        horizontalLayout->addWidget(universeSizeX);
+
+        label_2 = new QLabel(horizontalLayoutWidget);
+        label_2->setObjectName(QStringLiteral("label_2"));
+        label_2->setMinimumSize(QSize(25, 30));
+        label_2->setFont(font);
+
+        horizontalLayout->addWidget(label_2);
+
+        universeSizeY = new QSpinBox(horizontalLayoutWidget);
+        universeSizeY->setObjectName(QStringLiteral("universeSizeY"));
+        universeSizeY->setMinimumSize(QSize(100, 30));
+        universeSizeY->setValue(50);
+
+        horizontalLayout->addWidget(universeSizeY);
+
+        resizeButton = new QPushButton(horizontalLayoutWidget);
+        resizeButton->setObjectName(QStringLiteral("resizeButton"));
+        resizeButton->setMinimumSize(QSize(100, 30));
+        resizeButton->setMaximumSize(QSize(100, 30));
+
+        horizontalLayout->addWidget(resizeButton);
+
+        horizontalLayoutWidget_2 = new QWidget(centralWidget);
+        horizontalLayoutWidget_2->setObjectName(QStringLiteral("horizontalLayoutWidget_2"));
+        horizontalLayoutWidget_2->setGeometry(QRect(5, 40, 441, 41));
+        horizontalLayout_2 = new QHBoxLayout(horizontalLayoutWidget_2);
+        horizontalLayout_2->setSpacing(6);
+        horizontalLayout_2->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout_2->setObjectName(QStringLiteral("horizontalLayout_2"));
+        horizontalLayout_2->setContentsMargins(0, 0, 0, 0);
+        runButton = new QPushButton(horizontalLayoutWidget_2);
         runButton->setObjectName(QStringLiteral("runButton"));
-        runButton->setGeometry(QRect(10, 10, 100, 30));
         runButton->setMinimumSize(QSize(100, 30));
         runButton->setMaximumSize(QSize(100, 30));
-        nextButton = new QPushButton(centralWidget);
+
+        horizontalLayout_2->addWidget(runButton);
+
+        nextButton = new QPushButton(horizontalLayoutWidget_2);
         nextButton->setObjectName(QStringLiteral("nextButton"));
-        nextButton->setGeometry(QRect(120, 10, 100, 30));
         nextButton->setMinimumSize(QSize(100, 30));
         nextButton->setMaximumSize(QSize(100, 30));
+
+        horizontalLayout_2->addWidget(nextButton);
+
+        stopButton = new QPushButton(horizontalLayoutWidget_2);
+        stopButton->setObjectName(QStringLiteral("stopButton"));
+        stopButton->setMinimumSize(QSize(100, 30));
+        stopButton->setMaximumSize(QSize(100, 30));
+
+        horizontalLayout_2->addWidget(stopButton);
+
+        clearButton = new QPushButton(horizontalLayoutWidget_2);
+        clearButton->setObjectName(QStringLiteral("clearButton"));
+        clearButton->setMinimumSize(QSize(100, 30));
+        clearButton->setMaximumSize(QSize(100, 30));
+
+        horizontalLayout_2->addWidget(clearButton);
+
         GameOfLifeViewClass->setCentralWidget(centralWidget);
+        horizontalLayoutWidget->raise();
+        universeSizeX->raise();
+        horizontalLayoutWidget_2->raise();
         menuBar = new QMenuBar(GameOfLifeViewClass);
         menuBar->setObjectName(QStringLiteral("menuBar"));
-        menuBar->setGeometry(QRect(0, 0, 800, 21));
+        menuBar->setGeometry(QRect(0, 0, 450, 21));
         menuFile = new QMenu(menuBar);
         menuFile->setObjectName(QStringLiteral("menuFile"));
         GameOfLifeViewClass->setMenuBar(menuBar);
@@ -98,14 +189,19 @@ public:
 
     void retranslateUi(QMainWindow *GameOfLifeViewClass)
     {
-        GameOfLifeViewClass->setWindowTitle(QApplication::translate("GameOfLifeViewClass", "GameOfLifeView", 0));
+        GameOfLifeViewClass->setWindowTitle(QApplication::translate("GameOfLifeViewClass", "Game of Life", 0));
         actionNew->setText(QApplication::translate("GameOfLifeViewClass", "New", 0));
         actionOpen->setText(QApplication::translate("GameOfLifeViewClass", "Open", 0));
         actionSave->setText(QApplication::translate("GameOfLifeViewClass", "Save", 0));
         actionSave_As->setText(QApplication::translate("GameOfLifeViewClass", "Save As", 0));
         actionQuit->setText(QApplication::translate("GameOfLifeViewClass", "Quit", 0));
+        label->setText(QApplication::translate("GameOfLifeViewClass", "row", 0));
+        label_2->setText(QApplication::translate("GameOfLifeViewClass", "col", 0));
+        resizeButton->setText(QApplication::translate("GameOfLifeViewClass", "resize", 0));
         runButton->setText(QApplication::translate("GameOfLifeViewClass", "run", 0));
         nextButton->setText(QApplication::translate("GameOfLifeViewClass", "next", 0));
+        stopButton->setText(QApplication::translate("GameOfLifeViewClass", "stop", 0));
+        clearButton->setText(QApplication::translate("GameOfLifeViewClass", "clear", 0));
         menuFile->setTitle(QApplication::translate("GameOfLifeViewClass", "File", 0));
     } // retranslateUi
 
